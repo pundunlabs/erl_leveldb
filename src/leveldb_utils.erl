d6ac7f841c0f85591c5afded737214387a282daa
jonasf <jonas.falkevik@mobilearts.com>
2015-03-02 17:48:30 +0100

    Add test functions to leveldb_utils

--- src/leveldb_utils.erl
+++ src/leveldb_utils.erl
@@ -2,6 +2,13 @@
 
 -export([merge_sorted_kvls/1]).
 
+-export([test/1,
+	 test_lists/1,
+	 test_lists2/1,
+	 test_get_lists/2,
+	 test_get_lists/3,
+	 test_merge_lists/2]).
+
 -on_load(init/0).
 
 -type value() :: binary() | string().
@@ -32,6 +39,46 @@ Dir = "../priv",
 %% @end
 %%--------------------------------------------------------------------
 -spec merge_sorted_kvls(KVLs :: [[{key(), value()}]]) -> {ok, [{key(), value()}]} | {error, Reason :: any()}.
-merge_sorted_kvls(KVLs)->
+merge_sorted_kvls(_KVLs)->
     erlang:nif_error(nif_library_not_loaded).
 
+%%%===================================================================
+%%% API
+%%%===================================================================
+%%--------------------------------------------------------------------
+%% @doc Test functions.
+%% Tests for merge sorted key value lists.
+%% @end
+%%--------------------------------------------------------------------
+test(N) ->
+    [Odd, Even] = test_get_lists(1,N),
+    {[Odd, Even], merge_sorted_kvls([Odd, Even])}.
+
+test_lists(Lists) ->
+    timer:tc(?MODULE, merge_sorted_kvls, [Lists]).
+
+test_lists2(Lists) ->
+    timer:tc(?MODULE, test_merge_lists, [Lists, []]).
+
+test_get_lists(Start, Stop) ->
+    Odd  = test_get_modular_seq(Start, Stop, 2, 1),
+    Even = test_get_modular_seq(Start, Stop, 2, 0),
+    [Odd, Even].
+
+test_get_modular_seq(Start, Stop, Mod, Rem) ->
+    [ begin Str = list_to_binary(integer_to_list(X)),
+	   {Str,<<"Data">>} end || X <- lists:seq(Stop, Start, -1), X rem Mod == Rem].
+    
+
+test_get_lists(Start, Stop, Mod) ->
+    [test_get_modular_seq(Start, Stop, Mod, R)|| R <- lists:seq(0, Mod-1)].
+
+test_merge_lists([H | Rest], []) ->
+    test_merge_lists(Rest, H);
+test_merge_lists([H | Rest], Aux) ->
+    test_merge_lists(Rest, lists:keymerge(1, H, Aux));
+test_merge_lists([], Aux) ->
+    Aux.
+    
+    
+    
